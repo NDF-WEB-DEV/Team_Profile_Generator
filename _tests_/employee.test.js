d1c6a4e53e6688e8e779e74d5a26a6fa38455a5a
Nordleen De Frias <nordleendefrias@gmail.com>
2022-10-11 21:12:05 -0400

    Added the engineer.test.js

--- _tests_/employee.test.js
+++ _tests_/employee.test.js
@@ -2,10 +2,10 @@ const Employee = require('../lib/employee.js');
 
 describe("Employee", () => {
     it("should create an object of the employee class if provided with valid arguments", () => {
-        const employee = new employee("John","101","johnsmith@fakecompany.com");
+        const employee = new employee("John Smith","101","johnsmith@fakecompany.com");
 
         //test new object uses the correct parameters
-        expect(employee.name).toEqual("John");
+        expect(employee.name).toEqual("John Smith");
         expect(employee.id).toEqual("101");
         expect(employee.email).toEqual("johnsmith@fakecompany.com");
     });
@@ -23,11 +23,11 @@ describe("Employee", () => {
     //Test the getEmail method
     it("should return the email of the employee class when the getEmail method is called", () => {
         expect(employee.getEmail()).toEqual("johnsmith@fakecompany.com");
-    })
+    });
 
     //Test the getRole method
     it("should return the employee class object when the getRole method is called", () => {
         expect(employee.getRole()).toEqual('Employee');
-    })
+    });
 });
 
---
+++ _tests_/engineer.test.js
@@ -0,0 +1,38 @@
+const Engineer = require("../lib/engineer");
+
+describe("Engineer", () => {
+    it("should create an object of the engineer class if provided with valid arguments", () => {
+        const engineer = new Engineer("Ari Green","102","arigreen@fakecompany.com", "NDF-WEB-DEV");
+        
+        //test new object uses the correct parameters
+        expect(engineer.name).toEqual("Ari Green");
+        expect(engineer.id).toEqual("102");
+        expect(engineer.email).toEqual("arigreen@fakecompany.com");
+        expect(engineer.githubUser).toEqual("NDF-WEB-DEV");
+    });
+
+    //Test the getName method()
+    it("should return the name of the engineer class when the getName method is called", () => {
+        expect(engineer.getName()).toEqual("Ari Green");
+    });
+
+    //Test the getId method
+    it("should return the id number of the engineer class when the getId method is called", () => {
+        expect(engineer.getId()).toEqual("102");
+    });
+
+    //Test the getEmail method
+    it("should return the email of the engineer class when the getEmail method is called", () => {
+        expect(engineer.getEmail()).toEqual("arigreen@fakecompany.com");
+    })
+    
+    //Test the getGithub method
+    it("should return the GitHub username of the engineer class when the getGithub method is called", () => {
+        expect(engineer.getGithub()).toEqual("NDF-WEB-DEV");
+    })
+
+    //Test the getRole method
+    it("should return the engineer class object when the getRole method is called", () => {
+        expect(engineer.getRole()).toEqual("Engineer");
+    });
+});
